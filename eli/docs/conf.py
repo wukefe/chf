83172ce96a9988ff87c1afea3b6e73e016abd270
wukefe <wukefe@gmail.com>
2016-08-06 12:22:12 -0400

    Move sidebar to left

--- eli/docs/conf.py
+++ eli/docs/conf.py
@@ -131,7 +131,7 @@ html_theme = 'bizstyle'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "rightsidebar": "true"
+    "rightsidebar": "false"
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
