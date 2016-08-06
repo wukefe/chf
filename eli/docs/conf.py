98f7c1c8f973e11ba8f3c8360d9277786551e58a
wukefe <wukefe@gmail.com>
2016-08-05 23:31:42 -0400

    Change theme

--- eli/docs/conf.py
+++ eli/docs/conf.py
@@ -114,7 +114,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'bizstyle'
 # sphinx_rtd_theme
 # nature
 # bizstyle
